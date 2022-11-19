19da29ccf90ac55183a9fce4399e05852ce021d0
Mihir Kadiya <mkadiya@uwo.ca>
2022-11-18 23:57:38 -0500

    initialised repository with basic chrome extension

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex w-full h-screen justify-center text-white bg-slate-700">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
